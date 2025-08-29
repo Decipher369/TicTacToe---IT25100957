55325e1342cc181c871621661c800b65a3e74583
Decipher <karthigesonshagash2005@gmail.com>
2025-08-29 14:30:00 +0530

    Initial commit - basic structure with placeholder functions

---
+++ Main.c
@@ -0,0 +1,33 @@
+/*
+ * Tic-Tac-Toe Game
+ * Started working on this today
+ * Not sure how to implement everything yet
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Welcome to Tic-Tac-Toe!\n");
+    printf("This is just a placeholder for now\n");
+    
+    // TODO: add game logic here
+    // TODO: figure out how to make the board
+    // TODO: add player input
+    
+    return 0;
+}
+
+// trying to make a function to check if someone won
+// not sure if this is right
+int checkWin() {
+    // this function is empty for now
+    // need to figure out how to check rows, columns, diagonals
+    return 0;
+}
+
+// function to display board
+// not implemented yet
+void displayBoard() {
+    printf("Board display not ready yet\n");
+}
